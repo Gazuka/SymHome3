a3da9c82e9965d2b83a59916fdfea19b0d61fb74
Gazuka <jerome.carion@gmail.com>
2019-05-16 07:49:20 +0200

    Préparation des users

--- src/Controller/GeneralController.php
+++ src/Controller/GeneralController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Utilisateur;
+use App\Form\UtilisateurType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class GeneralController extends AbstractController
 {
@@ -16,4 +21,114 @@ class GeneralController extends AbstractController
             'controller_name' => 'GeneralController',
         ]);
     }
+
+    /** FONCTIONS DE SIMPLIFICATION DU CODE ***********************************************************************************************************************************************/
+    /**
+     * Création d'un formulaire pour un nouveau element (objet entity)
+     * @return Response
+     */
+    private function creerElement($element, $request, $manager, $class, $pagedebase, $pagederesultat, $titre, $dependances = null):Response{
+        $form = $this->createForm($class, $element);        
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($element);
+            //Persist des dependances
+            if($dependances != null)
+            {
+                foreach($dependances as $dependance => $elem)
+                {
+                    eval('$objets = $element->get'.$dependance.'();');
+                    foreach($objets as $objet)
+                    {
+                        eval('$objet->set'.$elem.'($element);');
+                        $manager->persist($objet);
+                    }
+                }
+            }  
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                "L'élément : <strong>{$element->getNom()}</strong> a bien été créé !"
+            );
+            //Affichage de la liste des elements apres l'ajout du nouveau
+            return $this->redirectToRoute($pagederesultat);
+        }
+        //Affichage de la page avec le formulaire
+        return $this->render($pagedebase, [
+            'form' => $form->createView(),
+            'titre' => $titre
+        ]);
+    }
+
+    /**
+     * Affichage de l'ensemble des éléments
+     *
+     * @return Response
+     */
+    private function recupererElements($repo, $elements, $titre, $pagederesultat):Response {
+
+        $recup = $repo->findAll();
+
+        return $this->render($pagederesultat, [
+            'titre' => $titre,
+            $elements => $recup
+        ]);
+    }
+
+    /**
+     * Permet d'afficher le formulaire d'édition d'un élément
+     * @return Response
+     */
+    public function editElement($element, $classType, $pagederesultat, $request, $manager, $dependances = null):Response {
+        $form = $this->createForm($classType, $element);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($element);
+            //Persist des dependances
+            if($dependances != null)
+            {
+                foreach($dependances as $dependance => $elem)
+                {
+                    eval('$objets = $element->get'.$dependance.'();');
+                    foreach($objets as $objet)
+                    {
+                        eval('$objet->set'.$elem.'($element);');
+                        $manager->persist($objet);
+                    }
+                }
+            }  
+
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'élément' <strong>{$element->getNom()}</strong> a bien été enregistrée !"
+            );
+        }
+
+        return $this->render($pagederesultat, [
+            'element' => $element,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /** GESTION DES UTILISATEURS ******************************************************************************************************************************************************/
+    /**
+     * Création d'un utilisateur
+     * 
+     * @Route("/utilisateur/new", name="general_utilisateur_new")
+     *
+     * @return Response
+     */
+    public function creerUtilisateur(Request $request, ObjectManager $manager):Response {
+        $element = new Utilisateur();
+        $class = UtilisateurType::class;
+        $pagedebase = 'cuisine/element_new.html.twig';
+        $pagederesultat = 'cuisine';
+        $titre = "Création d'un utilisateur";
+        return $this->creerElement($element, $request, $manager, $class, $pagedebase, $pagederesultat, $titre);
+    } 
 }
---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UtilisateurRepository")
+ */
+class Utilisateur implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pseudo;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $photo;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $hash;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
+    //public function __construct(UserPasswordEncoderInterface $encoder)
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): self
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getHash(): ?string
+    {
+        return $this->hash;
+    }
+
+    public function setHash(string $hash): self
+    {
+        $this->hash = $hash;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getPassword()
+    {
+        return $this->hash;
+    }
+
+    public function getSalt(){}
+
+    public function getUsername()
+    {
+        return $this->pseudo;
+    }
+
+    public function eraseCredentials(){}
+}
