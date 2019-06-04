fc350a7e679d2e5c2b64f681d9e7ff2fc3bc9430
Gazuka <jerome.carion@gmail.com>
2019-06-04 23:49:41 +0200

    Amélioration des formulaires

--- src/Form/RecetteType.php
+++ src/Form/RecetteType.php
@@ -3,32 +3,37 @@
 namespace App\Form;
 
 use App\Entity\Recette;
+use App\Form\ConfigType;
 use App\Form\IngredientType;
 use App\Form\EtapeRecetteType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
-class RecetteType extends AbstractType
+class RecetteType extends ConfigType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('portion')
+            ->add('nom', TextType::class, $this->getConfiguration("Nom", "Quel est le nom de votre recette ?"))
+            ->add('portion', IntegerType::class, $this->getConfiguration("Portion", "Pour combien de personnes est prévue cette recette ?"))
             ->add('ustensiles')
             ->add('ingredients', CollectionType::class,
             [
                 'entry_type' => IngredientType::class,
                 'allow_add' => true,
-                'allow_delete' => true
+                'allow_delete' => true,
+                'label' => "Liste des ingrédients :"
             ])
             ->add('etapesRecette', CollectionType::class,
             [
                 'entry_type' => EtapeRecetteType::class,
                 'allow_add' => true,
-                'allow_delete' => true
+                'allow_delete' => true,
+                'label' => "Etapes de réalisation de la recette :"
             ])            
         ;
     }
