d27f1030f4dc94fc423b9d3c5a58ff2f7da627e5
Gazuka <jerome.carion@gmail.com>
2019-07-14 23:28:59 +0200

    Google Home init

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,51 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\Fulfillment;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
      */
-    public function index()
+    public function index(Fulfillment $fulfillment, ObjectManager $manager)
     {
-        return $this->render('home/index.json.twig', [
+        $fulfillment->index();
+
+        //On ajoute des variables selon la page de redirection
+        switch($fulfillment->Action)
+        {
+            /*case 'liste_boites':                
+                //$age = $fulfillment->fulfillmentRecupContext('patate', 'age');
+                $repo = $manager->getRepository(Boite::class);
+                return $this->listingBoites($repo);
+            break;
+            case 'liste_typealiment':
+                //Affiche la liste des type d'aliment
+                $repo = $manager->getRepository(TypeAliment::class);
+                return $this->listingTypesAliment($repo);
+            break;
+            case 'new_typealiment';
+                $nom = $fulfillment->fulfillmentRecupParam('nom');
+                $typealiment = new TypeAliment();
+                $typealiment->setNom($nom);
+                $manager->persist($typealiment);
+                $manager->flush();
+                return $this->render('cuisine/newtypealiment.'.$this->format.'.twig', [
+                    'nom' => $nom
+                ]);
+            break;*/
+            default :
+                //$this->format = "html";
+                return $this->render('home/index.json.twig');
+            break;
+        }
+
+        /*return $this->render('home/index.json.twig', [
             'controller_name' => 'HomeController',
-        ]);
+        ]);*/
     }
 }
---
+++ src/Service/Fulfillment.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\TypeAliment;
+
+class Fulfillment {
+    private $Json;
+    public $Action;
+
+    public function index()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method == 'POST'){$fichier = 'php://input';}else{$fichier = 'test.json';}//fichier reel ou fichier fictif pour dev
+
+        //Récupération du fichier Json
+        $this->fulfillment($fichier);
+        //Appel de la fonction demandé
+        //return $this->fulfillmentTransformAction();
+        //return $this->fulfillmentRepondre(); 
+        dump($this->Action);
+    }
+
+    /**
+     * Permet de récupérer le Json d'un Fulfillment DialogFlow
+     */
+    private function fulfillment($fichier)
+    {
+        //Récuperation du json
+        $requestBody = file_get_contents($fichier);
+        $json = json_decode($requestBody);
+        $this->Json = $json;
+
+        //Récupération de la variable Action dans le json
+        $this->Action = $this->Json->queryResult->action;
+    }
+
+    /**
+     * On récupére une variable du ContextOutput de Dialogflow
+     *
+     * @return void
+     */
+    public function fulfillmentRecupContext($context, $variable)
+    {
+        foreach($this->Json->queryResult->outputContexts as $param)   
+        {
+            $tab = explode("/", $param->name);
+            $ContextName = $tab[count($tab)-1];
+            if($context == $ContextName)
+            {
+                return $param->parameters->$variable;
+            }            
+        } 
+    }
+  
+    /**
+     * On récupére une variable parametre de Dialogflow
+     *
+     * @return void
+     */
+    public function fulfillmentRecupParam($variable)
+    {
+        return $this->Json->queryResult->parameters->$variable;
+    }
+}
